52f5bbfcf986d8930542c5ca455ef4386cd88139
AdrianDias444 <adriandias0709@gmail.com>
2025-11-23 19:30:53 +0000

    gnome sort done

--- gnome_sort/gnome_sort.py
+++ gnome_sort/gnome_sort.py
@@ -1,19 +1,41 @@
 def gnome_sort(array):
     i = 1
-    while (i < len(array)):
-        if(i == 0):
+    while i < len(array):
+        if i == 0:
             i = 1
-        j = i -1
-        if(array[j] > array[i]):
+        j = i - 1
+        if array[j] > array[i]:
             array[j], array[i] = array[i], array[j]
-            i = i-1
+            i = i - 1
         else:
-            i = i+1
+            i = i + 1
 
 
-
-
-array = [33,201,89,242,142,199,12,54,161,177,23,212,112,155,188,74,2,44,222,64,92,239,108]
+array = [
+    33,
+    201,
+    89,
+    242,
+    142,
+    199,
+    12,
+    54,
+    161,
+    177,
+    23,
+    212,
+    112,
+    155,
+    188,
+    74,
+    2,
+    44,
+    222,
+    64,
+    92,
+    239,
+    108,
+]
 gnome_sort(array)
 for i in range(len(array)):
     print(array[i])
